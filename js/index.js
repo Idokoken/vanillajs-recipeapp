e3c9287f09780ab0ed5c65352695f921af246743
idokoken <idokokennethnduka@gmail.com>
2022-05-19 21:54:21 +0100

    updated

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const submit = document.getElementById('submit')
 const search = document.getElementById('search') 
 const random = document.getElementById('random') 
 const mealHeading = document.getElementById('mealHeading')
-const meals = document.getElementById('meals') 
+const mealsEl = document.getElementById('meals') 
 const single_meal = document.getElementById('single-meal') 
  
 // search and find meal
@@ -16,7 +16,7 @@ single_meal.innerHTML = ''
 //check for empty input
 if(input.trim()) {
 	try{
-		const res = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' +- input)
+		const res = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + input)
 		const data = await res.json()
 		 console.log(data)
 		 mealHeading.innerHTML = `<h2>Meal Result: ${input}</h2>`
@@ -38,6 +38,47 @@ if(input.trim()) {
 }
 	alert('enter meal')
 	
+
+
 }
 //handle meal submit
-submit.addEventListener('click', searchMeal)
+submit.addEventListener('submit', searchMeal)
+
+//single meal function
+async function getMealId(mealId) {
+	try{
+		const res = await fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=' + mealId)
+		const data = await res.json()
+		const meal = data.meals[0]
+		addMealToDom(meal)
+	}catch(err){
+		threw new Error()
+	}
+}
+//function add meal to dom
+function addMealToDom(meal){
+	const ingridents = []
+	for(let i = 1; i <= 20; i++) {
+		if(meal[`strIngrident${i}`]) {
+			ingridents.push(`${meal[`strIngrident${i}`]}`)
+		} else {
+			
+		}
+	}
+}
+
+// get single meal
+
+mealsEl.addEventListener('click', (e) => {
+	 const mealInfo = e.path.find(item => {
+	 	if(item.classList) {
+	 		return item.classList.contains('meal-info')
+	 	} else {
+	 		return false
+	 	}
+	 })
+	 if(mealInfo) {
+	 	const mealId = mealInfo.getAttribute('data-mealid')
+	 	getMealId(mealId)
+	 }
+})
